48ea21cb6d459fae66db12b102910f44c4fb9e5b
Olga Larina <21apr1988@gmail.com>
2025-02-17 20:28:39 +0200

    added pages to rout

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from 'react-router';
-import App from './App';
+import Home from './view/pages/Home/Home';
+import LoginRegister from './view/pages/loginRegister/LoginRegister';
+import AdminPanel from './view/pages/adminPanel/AdminPanel';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <App />,
+		element: <Home />,
 	},
+	{
+		path: '/login-register',
+		element: <LoginRegister />,
+	},
+	{
+		path: 'admin-panel',
+		element: <AdminPanel />,
+	}
 ]);
