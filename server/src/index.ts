845cb3b076344f27b83e4b081779b13f8e24b225
MayaLY <maya_levy@yahoo.com>
2025-02-17 20:58:57 +0200

    sql connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import { testConnection } from './db'
 const app = express()
 const port = 3000
 
 app.get('/', (req:any, res:any) => {
   res.send('Hello World!')
 })
+
+testConnection();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
