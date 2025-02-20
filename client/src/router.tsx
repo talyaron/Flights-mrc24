131b357e0bb43c75f4238cd6a223c5013adbdb9a
Tal Yaron <tal.yaron@gmail.com>
2025-02-20 18:45:13 +0200

    changed router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,12 +2,9 @@ import { createBrowserRouter } from 'react-router';
 import Home from './view/pages/Home/Home';
 import LoginRegister from './view/pages/loginRegister/LoginRegister';
 import AdminPanel from './view/pages/adminPanel/AdminPanel';
-
+import Company from './view/pages/company/Company';
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Home />,
-	},
+	
 	{
 		path: '/login-register',
 		element: <LoginRegister />,
@@ -15,5 +12,19 @@ export const router = createBrowserRouter([
 	{
 		path: 'admin-panel',
 		element: <AdminPanel />,
-	}
+	},
+	{
+		path: 'company',
+		element: <Company />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: 'admin-panel',
+				element: <AdminPanel />,
+			},
+		],
+	},
 ]);
