3ef2e54af2dd2035200a9abd301424cb4d80a05a
MayaLY <maya_levy@yahoo.com>
2025-02-24 20:23:47 +0200

    server file updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,22 @@
-// import express from 'express'
-// import pool from './db'
-// import cors from "cors";
-// import "dotenv/config";
-// import cookieParser from "cookie-parser";
-// import usersRouter from "./routes/users/usersRoutes";
-// import flightsRouter from './routes/flights/flightsRouter';
+import express from 'express'
+import pool from './db'
+import usersRouter from './routes/users/usersRouter';
+import adminRouter from './routes/sysAdmin/adminRouter';
+const app = express()
+const port = 3000
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+pool.getConnection()
+const apiRouter = express.Router();
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/admin", adminRouter);
+
+app.use("/api", apiRouter);
 
 
-
-
-// const app = express()
-// const port = 3000
-
-// //middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], // Your React app origin
-//     credentials: true, // Allow cookies to be sent with requests
-//   })
-// );
-
-// // 🔹 Test Connection
-// pool.getConnection()
-//   .then(() => console.log("✅ Connected to MySQL Database"))
-//   .catch((err) => console.error("❌ MySQL Connection Error:", err));
-
-// const apiRouter = express.Router();
-// apiRouter.use("/users", usersRouter);
-// apiRouter.use("/flights", flightsRouter);
-
-// app.use("/api", apiRouter);
-
-// app.get("/", (req: any, res: any) => {
-//   res.send("Hello World!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
