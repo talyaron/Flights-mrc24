749169b21497fb963572df2782aad4b2d08ff6c9
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2025-02-24 17:37:05 +0200

    changes

--- client/src/view/pages/company/FlightActions.tsx
+++ client/src/view/pages/company/FlightActions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 const FlightActions: React.FC = () => {
     const navigate = useNavigate();
