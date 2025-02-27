4617a48dfb54d2d9e1ce3f82703f4ff188b489bd
HotNuggets <mishamandark@gmail.com>
2025-02-27 18:48:17 +0200

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express'
 import pool from './db'
-import usersRouter from './routes/users/usersRouter';
+import usersRouter from './routes/users/usersRouters';
 import adminRouter from './routes/sysAdmin/adminRouter';
+import cors from 'cors';
 const app = express()
 const port = 3000
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+};
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], // Your React app origin
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
 app.get('/', (req:any, res:any) => {
   res.send('Hello World!')
 })
