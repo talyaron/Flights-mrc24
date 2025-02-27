c919b987f21dd21871676e8d0e8335903e5ae58c
israelcohen1 <134219173+israelcohen1@users.noreply.github.com>
2025-02-27 14:59:01 +0200

    fixed

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,19 +1,22 @@
 import mysql from "mysql2";
-import { config } from "dotenv";
+import {config} from 'dotenv';
 config();
+
+const password = process.env.MYSQL_PASSWORD;
 
 
 const pool = mysql
   .createPool({
     host: "localhost",
     user: "root",
-    password: process.env.MYSQL_PASSWORD,
+    password: password,
     database: "flights", 
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
   })
   .promise();
+
 
 
 export default pool;
