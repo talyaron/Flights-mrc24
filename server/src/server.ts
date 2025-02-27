58c31daaca683cbe76e1fa29b9fbb65763e93ad4
Tal Yaron <tal.yaron@gmail.com>
2025-02-27 18:23:32 +0200

    fixed issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import pool from './db'
-import usersRouter from './routes/users/usersRouter';
+import usersRouter from './routes/users/usersRouters';
 import adminRouter from './routes/sysAdmin/adminRouter';
+import cors from 'cors';
 const app = express()
-const port = 3000
+const port = 3000;
 
-app.get('/', (req:any, res:any) => {
-  res.send('Hello World!')
-})
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 pool.getConnection()
 const apiRouter = express.Router();
