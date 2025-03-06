42f877ca6365107a8c58fe9ede97c3c733989fdf
MayaLY <maya_levy@yahoo.com>
2025-03-06 20:31:11 +0200

    waiting navlink

--- client/src/view/pages/company/companyNav/CompanyNav.tsx
+++ client/src/view/pages/company/companyNav/CompanyNav.tsx
@@ -5,6 +5,7 @@ const CompanyNav = () => {
   return (
     <div className={styles.companyNav}>
         <NavLink to="/company" activeClassName={styles.active} end>Home</NavLink>
+        <NavLink to="/company/waiting" activeClassName={styles.active} end>Waiting</NavLink>
         <NavLink to="/company/admin-panel" activeClassName={styles.active}>Admin Panel</NavLink>
         <NavLink to="/company/set-flights" activeClassName={styles.active}>Set Flights</NavLink>
         <NavLink to="/company/add-flight" activeClassName={styles.active}>Add Flight</NavLink>
